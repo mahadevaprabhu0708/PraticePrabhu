efdb9b39a8a414c68c754b02b9598c9084db8e67
mahadg <mahadg@USBLRMAHADG2.us.deloitte.com>
2024-11-05 08:52:08 +0530

    Second Push

---
+++ DummayPro/src/test/java/ToCreateLibraryTest/ToCreateLibraryTest.java
@@ -0,0 +1,11 @@
+package ToCreateLibraryTest;
+
+public class ToCreateLibraryTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Library is created successfully");
+
+	}
+
+}
---
+++ DummayPro/src/test/java/ToCreateShopingTest/ToCreateShopingTest.java
@@ -0,0 +1,10 @@
+package ToCreateShopingTest;
+
+public class ToCreateShopingTest {
+
+	public static void main(String[] args) {
+		System.out.println("Shoping is created successfully");
+
+	}
+
+}
